4ae0b21193b82a40e0f62b73730b68c69fcb0edf
sandywyper <sandywyper@gmail.com>
2020-05-04 15:18:04 +0100

    Layout and content changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,13 +5,30 @@ import Scroll from './Scroll'
 const Nav = props => (
   <nav id="nav" className={props.sticky ? 'alt' : ''}>
     <Scrollspy
-      items={['where-and-when', 'travel', 'accommodation', 'RSVP', 'more-info']}
+      items={[
+        'where-and-when',
+        'RSVP',
+        'venue',
+        'travel',
+        'accommodation',
+        'more-info',
+      ]}
       currentClassName="is-active"
       offset={-300}
     >
       <li>
         <Scroll type="id" element="where-and-when">
           <a href="#">Where and when</a>
+        </Scroll>
+      </li>
+      <li>
+        <Scroll type="id" element="RSVP">
+          <a href="#">RSVP</a>
+        </Scroll>
+      </li>
+      <li>
+        <Scroll type="id" element="venue">
+          <a href="#">Venue</a>
         </Scroll>
       </li>
       <li>
@@ -22,11 +39,6 @@ const Nav = props => (
       <li>
         <Scroll type="id" element="accommodation">
           <a href="#">Accommodation</a>
-        </Scroll>
-      </li>
-      <li>
-        <Scroll type="id" element="RSVP">
-          <a href="#">RSVP</a>
         </Scroll>
       </li>
       <li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import { Waypoint } from 'react-waypoint'
 // import pic01 from '../assets/images/all-smart.png'
 // import pic02 from '../assets/images/[S+J].png'
 // import pic03 from '../assets/images/US2.png'
+import tipiOne from '../assets/images/tipi-1.jpg'
+import tipiTwo from '../assets/images/tipi-2.jpg'
+import bedSteps from '../assets/images/bed-steps.jpg'
 import pic04 from '../assets/images/smootchin.jpg'
 import Header from '../components/Header'
 import Layout from '../components/layout'
@@ -55,13 +58,28 @@ class Index extends React.Component {
             <div className="spotlight">
               <div className="content">
                 <header className="major">
-                  <h2>Saturday 5th June 2021</h2>
+                  <h2>Come &amp; Celebrate</h2>
+                  <p>
+                    We cordially invite you to join us for a day of
+                    celebrations, to witness the marrage of Miss Jennifer
+                    Simpson and young Master Alexander Wyper.
+                  </p>
                 </header>
                 <p>
-                  We ask all guests to arrive at 1.30pm; the ceremony will start
-                  at 2.00pm.
+                  We would be honoured by your presence as we tie the knot,
+                  enjoying wonderful company in a wonderful location, at 1:30pm
+                  on Saturday 5th, June 2021. Sat atop the cliffs on this
+                  stunning bit of coast line, the venue holds a special place
+                  for both of us, and we cannot think of a better place to share
+                  our big day with you all. There will be celebrations, feasting
+                  and topped off with a ceilidh, so bring your dancing shoes!
                 </p>
-                <p>
+                <div className="venue-shots">
+                  <img src={tipiOne} alt="Tipi Tent" />
+                  <img src={bedSteps} alt="Bedruthen Steps Beach" />
+                </div>
+
+                {/* <p>
                   The wedding ceremony and reception will take place at
                   Bedruthan Steps camping field, just outside the village of
                   Mawgan Porth, near Newquay, Cornwall. This is a gently sloping
@@ -78,7 +96,7 @@ class Index extends React.Component {
                   serviced by local public transport so you will need to either
                   drive or take a taxi to the venue. We will be providing
                   transport back to Newquay throughout the evening from 7pm.
-                </p>
+                </p> */}
                 {/* <ul className="actions">
                   <li>
                     <Link to="/generic" className="button">
@@ -91,6 +109,163 @@ class Index extends React.Component {
                 <img src={pic04} alt="Jenny and Sandy" />
               </span>
             </div>
+          </section>
+
+          <section id="RSVP" className="main special">
+            <header className="major">
+              <h2>RSVP</h2>
+            </header>
+            <form
+              method="post"
+              name="RSVP"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              action="/thank-you/"
+            >
+              <input type="hidden" name="bot-field" value="RSVP" />
+              <div className="field">
+                <label htmlFor="names">Name(s) on the invitation - </label>
+                <input
+                  type="text"
+                  name="names"
+                  id="names"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="field">
+                <label htmlFor="attending">Please select one - </label>
+                <select
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  name="attending"
+                  id="attending"
+                  required
+                >
+                  <option value="undefined">please select an option</option>
+                  <option value="All will attend">We'll be there!</option>
+                  <option value="We can't make it">
+                    Sorry, we can't make it.
+                  </option>
+                  <option value="Some can't make it">
+                    Some of us can't make it.
+                  </option>
+                </select>
+              </div>
+              {this.state.attending === "Some can't make it" && (
+                <div className="field">
+                  <label htmlFor="thoseAttending">
+                    Those who will be attending -
+                  </label>
+                  <input
+                    type="text"
+                    name="thoseAttending"
+                    id="thoseAttending"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              )}
+
+              <div className="field">
+                <label htmlFor="additionalInfo">
+                  If you have any dietary requirements, please state what they
+                  are and who they apply to. If you have small children that
+                  need a highchair, we can rent one for you, just let us know
+                  how many you need.
+                </label>
+                <textarea
+                  name="additionalInfo"
+                  id="additionalInfo"
+                  rows="4"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                ></textarea>
+              </div>
+              <div className="field-check">
+                <label htmlFor="camping">
+                  Will you be camping on site? There's loads of room, and you
+                  can change your mind nearer the time, but we'd like to get a
+                  feel for how many campers will be joining us.
+                </label>
+                <input
+                  type="checkbox"
+                  name="camping"
+                  id="camping"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                ></input>
+              </div>
+
+              <div className="field half">
+                <label htmlFor="email">
+                  Incase we need to get in touch with any of you nearer the
+                  time. Please leave your eamil address.
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <ul className="actions">
+                <li>
+                  <input type="submit" value="Send" className="special" />
+                </li>
+                <li>
+                  <input type="reset" value="Reset" />
+                </li>
+              </ul>
+            </form>
+          </section>
+
+          <section id="venue" className="main special">
+            <header className="major">
+              <h2>The Venue</h2>
+            </header>
+            <p className="text-left">
+              The wedding ceremony and reception will take place at Bedruthan
+              Steps camping field, just north of the village of Mawgan Porth,
+              near Newquay, Cornwall. You can join us in camping on site, or
+              there will be a shuttle bus running back and forth to Newquay, so
+              you may prefer accomodation along this route. We ask all guests to
+              arrive at 1.30pm, the ceremony will start at 2.00pm.
+            </p>
+            <div className="text-image-container">
+              <p>
+                Although this coast can have stunning weather it is rather
+                exposed, so we recomend coming prepared. It can be blistering
+                hot, or if the wind comes from the north it can be a bit chilly.
+                Especially in the evening, so make sure to bring layers. The
+                tents are huge, so if the heavens were to open then we should
+                all remain dry and have a great view all the same. If you're
+                camping, there are toilets, showers and running water. The field
+                is right above the beach, however there was a rock slide in the
+                winter so the steps remain closed at the moment. There's no
+                shortages of beaches nearby.
+              </p>
+              <img src={tipiTwo} alt="Wild Tipis" />
+            </div>
+            <p className="text-left">
+              The shutle bus will be running from around 7pm onwards, but we ask
+              that you make your own way to the venue. It's about a 15 minitue
+              drive from Newquay.
+            </p>
+            <ul className="actions">
+              <li>
+                <a
+                  href="https://www.google.com/maps/dir//50.4859372,-5.0299518/@50.4799437,-5.0225433,15z/data=!4m2!4m1!3e0?hl=en&authuser=0"
+                  className="button special"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Directions
+                </a>
+              </li>
+            </ul>
             <Map />
           </section>
 
@@ -111,7 +286,6 @@ class Index extends React.Component {
               <div className="travel-option">
                 <div className="travel-title">
                   <h3>By Air</h3>
-                  {/* <span className="travel-icon icon fa-plane"></span> */}
                 </div>
                 <div className="travel-body">
                   <p>
@@ -126,7 +300,6 @@ class Index extends React.Component {
               <div className="travel-option">
                 <div className="travel-title">
                   <h3>By Car</h3>
-                  {/* <span className="travel-icon icon fa-plane"></span> */}
                 </div>
                 <div className="travel-body">
                   <p>
@@ -251,117 +424,6 @@ class Index extends React.Component {
               minute drive from Newquay centre and not en route for transport
               back to Newquay on the evening of the wedding
             </p>
-          </section>
-
-          <section id="RSVP" className="main special">
-            <header className="major">
-              <h2>RSVP</h2>
-            </header>
-            <form
-              method="post"
-              name="RSVP"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              action="/thank-you/"
-            >
-              <input type="hidden" name="bot-field" value="RSVP" />
-              <div className="field">
-                <label htmlFor="names">Name(s) on the invitation - </label>
-                <input
-                  type="text"
-                  name="names"
-                  id="names"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                  required
-                />
-              </div>
-              <div className="field">
-                <label htmlFor="attending">Please select one - </label>
-                <select
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                  name="attending"
-                  id="attending"
-                  required
-                >
-                  <option value="undefined">please select an option</option>
-                  <option value="All will attend">We'll be there!</option>
-                  <option value="We can't make it">
-                    Sorry, we can't make it.
-                  </option>
-                  <option value="Some can't make it">
-                    Some of us can't make it.
-                  </option>
-                </select>
-              </div>
-              {this.state.attending === "Some can't make it" && (
-                <div className="field">
-                  <label htmlFor="thoseAttending">
-                    Those who will be attending -
-                  </label>
-                  <input
-                    type="text"
-                    name="thoseAttending"
-                    id="thoseAttending"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                  />
-                </div>
-              )}
-
-              <div className="field">
-                <label htmlFor="additionalInfo">
-                  If you have any dietary requirements, please state what they
-                  are and who they apply to. If you have small children that
-                  need a highchair, we can rent one for you, just let us know
-                  how many you need.
-                </label>
-                <textarea
-                  name="additionalInfo"
-                  id="additionalInfo"
-                  rows="4"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                ></textarea>
-              </div>
-              <div className="field-check">
-                <label htmlFor="camping">
-                  Will you be camping on site? There's loads of room, and you
-                  can change your mind nearer the time, but we'd like to get a
-                  feel for how many campers will be joining us.
-                </label>
-                <input
-                  type="checkbox"
-                  name="camping"
-                  id="camping"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                ></input>
-              </div>
-
-              <div className="field half">
-                <label htmlFor="email">
-                  Incase we need to get in touch with any of you nearer the
-                  time. Please leave your eamil address.
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <ul className="actions">
-                <li>
-                  <input type="submit" value="Send" className="special" />
-                </li>
-                <li>
-                  <input type="reset" value="Reset" />
-                </li>
-              </ul>
-            </form>
           </section>
 
           <section id="more-info" className="main">
